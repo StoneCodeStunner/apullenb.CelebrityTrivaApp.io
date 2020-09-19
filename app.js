62b1959c426c78e29650987d2b10d3501269fac9
LELAND STONE <lelandscottstone33@gmail.com>
2020-09-19 03:01:08 -0500

    Complete redo of all elements

--- app.js
+++ app.js
@@ -2,286 +2,204 @@
 /* eslint-disable no-console */
 /* eslint-disable no-undef */
 /* eslint-disable strict */
-const correctAnswerIcon = 'images/paul.jpg';
-const wrongAnswerIcon = 'images/jennifer.jpg';
-const warningIcon =
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSa7WP9E3LDA10kP2Rk2enw-_kjI-iJd4kny8yH2kTrPR8hgrwtBg';
-
-let questionCounter;
-questionCounter = 0;
-let score = 0;
-const questionsArray = [
-  {
-    question: '1. What Is Dwayne Johnsons Net Worth?',
-    optionone: '80 Million',
-    optiontwo: '120 Million',
-    optionthree: '250 Million',
-    optionfour: '320 Million',
-    correctAnswer: '320 Million',
-  },
-  {
-    question: '2. Which Year Did Paul Walker Die?',
-    optionone: '2013',
-    optiontwo: '2003',
-    optionthree: '2018',
-    optionfour: '2009',
-    correctAnswer: '2013',
-  },
-  {
-    question: '3. Who Sings The Chicka Chicka Slim Shady Song?',
-    optionone: 'Drake',
-    optiontwo: 'Lil Wayne',
-    optionthree: '50 Cent',
-    optionfour: 'Eminem',
-    correctAnswer: 'Eminem',
-  },
-  {
-    question: '4. What Is The Name Of The School In The Breakfast Club?',
-    optionone: 'Beverly High',
-    optiontwo: 'Shermer High',
-    optionthree: 'Houston County High',
-    optionfour: 'Montgomery High',
-    correctAnswer: 'Shermer High',
-  },
-  {
-    question: '5. In What Year Did Brad Pitt Turn 30 Years Old?',
-    optionone: '2003',
-    optiontwo: '2008',
-    optionthree: '1999',
-    optionfour: '2010',
-    correctAnswer: '2003',
-  },
-  {
-    question: '6. Which Celebrity Is Known As JLo?',
-    optionone: 'John Lennon',
-    optiontwo: 'Jennifer Lopez',
-    optionthree: 'Jared Leto',
-    optionfour: 'Justin Long',
-    correctAnswer: 'Jennifer Lopez',
-  },
-  {
-    question: '7. Who Was People Magazine Sexiest Man Alive in 1991?',
-    optionone: 'Brad Pitt',
-    optiontwo: 'Blake Shelton',
-    optionthree: 'Patrick Swayze',
-    optionfour: 'George Strait',
-    correctAnswer: 'Patrick Swayze',
-  },
-  {
-    question: '8. What Is Drew Barrymore\'s Real Middle Name?',
-    optionone: 'Beth',
-    optiontwo: 'Mckenzie',
-    optionthree: 'Leah',
-    optionfour: 'Blyth',
-    correctAnswer: 'Blyth',
-  },
-  {
-    question:
-      '9. How Old Was Russell Crowe When He Got His First Lead Role In A Film?',
-    optionone: '18',
-    optiontwo: '25',
-    optionthree: '20',
-    optionfour: '16',
-    correctAnswer: '25',
-  },
-  {
-    question: '10. Brothers Taylor, Isaac, Zac Make Up Which Band?',
-    optionone: 'ZZ Top',
-    optiontwo: 'The Police',
-    optionthree: 'Hanson',
-    optionfour: 'Green Day',
-    correctAnswer: 'Hanson',
-  },
-];
-
-let questionsCount = questionsArray.length;
-
-//this function handles the button that takes you to the first page of the quiz
-function handleStartClick() {
-  $('.js-start-button').on('click', function (event) {
-    console.log('handleStartClick() ran');
-    $('.progress-section').show();
-    $('.start-section').hide();
-    $('.end-section').hide();
-    $('.quiz-box').show();
-    renderQuizBox();
-  });
-}
-function renderQuizBox() {
-  // Displays the Question, answer choices, progress, and score
-  renderQuestionsCount();
-  renderQuestion();
-  renderScore();
-}
-function renderScore() {
-  // Calculates your score so far
-  $('.progress-section .score-card').text(`${score}/${questionsCount}`);
-}
-
-function renderQuestionsCount() {
-  // Displayed above the question and tells how many questions are left
-  $('.progress-section .question-count').text(
-    `Question ${questionCounter + 1} of ${questionsCount}`
-  );
-}
-
-function renderQuestion() {
-  // Displays the next question
-
-  $('.questions-form p').text(questionsArray[questionCounter].question);
-  $('.questions-form #optionone').val(
-    questionsArray[questionCounter].optionone
-  );
-  $('.questions-form #optiontwo').val(
-    questionsArray[questionCounter].optiontwo
-  );
-  $('.questions-form #optionthree').val(
-    questionsArray[questionCounter].optionthree
-  );
-  $('.questions-form #optionfour').val(
-    questionsArray[questionCounter].optionfour
-  );
-
-  $('.questions-form #optionone')
-    .next()
-    .text(questionsArray[questionCounter].optionone);
-  $('.questions-form #optiontwo')
-    .next()
-    .text(questionsArray[questionCounter].optiontwo);
-  $('.questions-form #optionthree')
-    .next()
-    .text(questionsArray[questionCounter].optionthree);
-  $('.questions-form #optionfour')
-    .next()
-    .text(questionsArray[questionCounter].optionfour);
-}
-
-function handleSubmitAnswer() {
-  // Submits and validates the selected option, displays popup, initiates the check answer function
-  $('.js-submit-button').on('click', function (event) {
-    console.log('handleSubmitAnswer() ran');
-    let selectedOption = $('input[type=radio]:checked').val();
-    if (selectedOption === undefined) {
-      displayPopup(false, selectedOption);
-    } else {
-      //reset radio button
-      $('input[type=radio]:checked').attr('checked', false);
-      checkAnswer(selectedOption);
+const STORE = {
+  questions: [
+    {
+      question: 'What Is Dwayne Johnsons Net Worth?',
+      answers: ['80 million', '120 million', '250 million', '320 million'],
+      correctAnswer: 3
+    },
+    {
+      question: 'What Year Did Paul Walker Pass Away?',
+      answers: ['2013', '2003', '2018', '2009'],
+      correctAnswer: 0
+    },
+    {
+      question: 'Who Sings The Chicka Chicka Slim Shady Song?',
+      answers: ['Drake', 'Lil Wayne', 'Eminem', '50 Cent'],
+      correctAnswer: 2
+    },
+    {
+      question: 'What Is The Name Of The School In The Breakfast Club?',
+      answers: ['Beverly High', 'Shermer high', 'Houston County High', 'Montgomery High'],
+      correctAnswer: 1
+    },
+    {
+      question: 'In What Year Did Brad Pitt Turn 30 Years Old?',
+      answers: ['2003', '2008', '1999', '2010'], correctAnswer: 0
+    },
+    {
+      question: 'Which Celebrity Is Known As JLo',
+      answers: ['John Lennon', 'Jennifer Lopez', 'Jared Leto', 'Justin Long'], correctAnswer: 1
+    },
+    {
+      question: 'Who Was Named People Magazines Sexiest Man Alive In 1991?',
+      answers: ['Brad Pitt', 'Blake Shelton', 'Patrick Swayze', 'George Strait'], correctAnswer: 2
+    },
+    {
+      question: 'What Is Drew Barrymores Real Middle Name?',
+      answers: ['Beth', 'Mckenzie', 'Leah', 'Blyth'], correctAnswer: 3
+    },
+    {
+      question: 'How Old Was Russell Crowe When He got His First Lead Role In A Film?',
+      answers: ['18', '25', '22', '16'], correctAnswer: 1
+    },
+    {
+      question: 'Brothers Taylor, Isaac, and Zac make up which Band?',
+      answers: ['ZZ Top', 'The Police', 'Hanson', 'Green Day'], correctAnswer: 2
     }
-  });
+    
+  ],
+  score: 0,
+  currentQuestion: 0,
+  guess: 0,
+  started: false,
+  hasFeedback: false
+};
+
+function generateQuestionElements(answers) {
+  let answerChoices = '';
+  answers.forEach((answer, i) => {
+  answerChoices += `<input type="radio" name="choice" value="${i}" id="${i}"/>
+  <label for="${i}">${answer}</label><br/>`;
+});
+return answerChoices;
 }
 
-function checkAnswer(selected) {
-  // Compares the input to the correct answer
-  // If the correct answer is selected, the popup with the correct answer is displayed
-  // If the wrong answer is selected, the incorrect answer popup is displayed with the correct answer
-  let rightAnswer = questionsArray[questionCounter].correctAnswer;
+function generateQuizElementsString(question, answers, score, totalQuestions, questionNumber) {
+  let options = generateQuestionElements(answers);
+  
+  return `<p class="score">Score: ${score}/${totalQuestions}</p>
+  <p class="progress">Questions ${questionNumber + 1} of ${totalQuestions}</p>
+  <section id="quiz">
+  <form>  
+    <h2>${question}</h2>
+    <fieldset id="choices" tabindex=0>${options}</fieldset>
+    <input type="submit" value="Submit Answer" aria-label="Submit Answer" />
+  </form>
+</section>`;
+}
 
-  if (selected === rightAnswer) {
-    score++;
-    displayPopup(true, rightAnswer);
+function generateStartElementString() {
+  return `<section id="start">
+  <h2>Hello! Welcome To Celebrity Trivia!</h2>
+  <button id="start-quiz">Start</button>
+</section>`;
+}
+
+function generateFeedbackElementString(feedback, guess, answer, score, totalQuestions, questionNumber) {
+  let incorrectStyle = '';
+  let guessString = `Your answer: ${guess}`;
+  let answerString = ``;
+  let output = `<p class="user-answer">${guessString}</p>
+  <p class="correct-answer">${answerString}</p>`;
+
+  if (feedback === 'Incorrect') {
+      incorrectStyle = `<p class="Incorrect"><img src="/images/jennifer.jpg">${answerString}</p>`
   } else {
-    displayPopup(false, rightAnswer);
+      output = `<p class="correct-answer"><img src="/images/paul.jpg">${answerString}</p>`;
   }
-}
-function displayPopup(statusFlag, answer) {
-  // Contains the code for displaying the popup
-  $('.feedback-section').show();
-  if (statusFlag) {
-    $('.popup-box img').attr('src', correctAnswerIcon);
-    $('.popup-box #popup-text').text('You are right!');
-    $('.popup-box').show();
-  } else {
-    if (answer === undefined) {
-      questionCounter--;
-      $('.popup-box img').attr('src', warningIcon);
-      $('.popup-box #popup-text').text('Please select an option');
-    } else {
-      $('.popup-box img').attr('src', wrongAnswerIcon);
-      $('.popup-box #popup-text').text(
-        `Sorry, the correct answer was: ${answer}`
-      );
-    }
-  }
-  $('.popup-box').show();
+
+  return `
+  <p class="score">Score: ${score}/${totalQuestions}</p>
+  <p class="progress">Question ${questionNumber + 1} of ${totalQuestions}</p>
+  <section id="feedback">
+  <h2> ${incorrectStyle}${feedback}</h2>
+  ${output}
+  <button id="next">Next Question</button>
+</section>`;
 }
 
-function handlePopupClose() {
-  //  Has a button to continue to the next question which displays the next question
-  // If it is the final question, the button will lead to the quiz results
-  $('.js-close-button').on('click', function (event) {
-    console.log('handlePopupClose() ran');
-    $('.popup-box').hide();
-    $('.feedback-section').hide();
-    $('.quiz-box').hide();
-    questionCounter++;
-    if (questionCounter < questionsArray.length) {
-      $('.quiz-box').show();
-      renderQuizBox();
+function generateSummaryElementString(score, totalQuestions) {
+  return `<section id="summary">
+  <h2>Summary</h2>
+  <p>Your score is ${score}/${totalQuestions}</p>
+  <button id="restart">Restart Quiz</button>
+</section>`;
+}
+
+function render() {
+  let score = STORE.score;
+  let totalQuestions = STORE.questions.length;
+  let questionNumber = STORE.currentQuestion;
+  let page = '';
+  console.log(STORE.currentQuestion);
+  console.log(STORE.questions.length);
+  if (!STORE.started) {
+    console.log(STORE.started);
+      page = generateStartElementString();
+    } else if (STORE.hasFeedback) {
+      console.log('aa');
+        let feedback = STORE.hasFeedback;
+        let guess = STORE.guess;
+        let answer = STORE.questions[STORE.currentQuestion].correctAnswer;
+      page = generateFeedbackElementString(feedback, guess, answer, score, totalQuestions, questionNumber);
+    } else if (STORE.currentQuestion < STORE.questions.length) {
+      console.log('aaa');
+        let question = STORE.questions[STORE.currentQuestion].question;
+        let answers = STORE.questions[STORE.currentQuestion].answers;
+        page = generateQuizElementsString(question, answers, score, totalQuestions, questionNumber);
     } else {
-      $('.quiz-box').hide();
-      displayFinalScore();
+      console.log('aaaa');
+      page = generateSummaryElementString(score, totalQuestions);
     }
-  });
+    $("main").html(page);
 }
-function displayFinalScore() {
-  // Displays the final score and has a button to restart the quiz
-  $('.end-section').show();
-  $('.end-section h4').text(`Your Score is: ${score}/${questionsCount}`);
-  $('.correct .count').text(score);
-  $('.wrong .count').text(questionsCount - score);
-  resetQuiz();
-}
-function resetQuiz() {
-  // Resets the score and progress counter
-  questionCounter = 0;
-  score = 0;
-}
-function handleStartOver() {
-  $('.js-startover-button').on('click', function (event) {
-    console.log('handleStartOver() ran');
-    $('.end-section').hide();
-    $('.quiz-box').show();
-    renderQuizBox();
+
+/* listening to events */
+function startQuiz() {
+  
+  $('main').on('click', '#start-quiz', ()  => {
+    console.log('aaab');
+    STORE.started = true;
+    render();
   });
 }
 
-function init() {
-  $('.end-section').hide();
-  $('.progress-section').hide();
-  $('.quiz-box').hide();
-  $('.feedback-section').hide();
-  handleStartClick();
-  handleSubmitAnswer();
-  handlePopupClose();
-  handleStartOver();
+function submitChoice() {
+  $('main').on ('submit', '#quiz form', e => {
+    e.preventDefault();
+    if (!$('input[type="radio"]:checked').val()) {
+      alert('No answer selected');
+      return;
+    }
+    const answer = $('input[type="radio"]:checked').val();
+    const question = STORE.questions[STORE.currentQuestion];
+    if (Number(answer) === question.correctAnswer) {
+      STORE.score = STORE.score + 1;
+      STORE.hasFeedback = 'Correct';
+    } else {
+      STORE.guess = STORE.questions[STORE.currentQuestion].answers[answer];
+      STORE.hasFeedback = 'Incorrect';
+    }
+    render();
+  });
 }
 
-$(init());
-/**
- *
- * Technical requirements:
- *
- * Your app should include a render() function, that regenerates the view each time the store is updated.
- * See your course material, consult your instructor, and reference the slides for more details.
- *
- * NO additional HTML elements should be added to the index.html file.
- *
- * You may add attributes (classes, ids, etc) to the existing HTML elements, or link stylesheets or additional scripts if necessary
- *
- * SEE BELOW FOR THE CATEGORIES OF THE TYPES OF FUNCTIONS YOU WILL BE CREATING 👇
- *
- */
+function nextQuestion() {
+  $('main').on ('click', '#next', () => {
+    STORE.hasFeedback = false;
+    STORE.currentQuestion = STORE.currentQuestion + 1;
+    render();
+  });
+}
 
-/********** TEMPLATE GENERATION FUNCTIONS **********/
+function restartQuiz() {
+  $('main').on ('click', '#restart', () => {
+    STORE.started = false;
+    STORE.score = 0;
+    STORE.currentQuestion = 0;
+    render();
+  });
+}
 
-//html template
-// contains the code for the question form and submit button
+function main() {
+  startQuiz();
+  submitChoice();
+  nextQuestion();
+  restartQuiz();
+  render();
+}
 
-/********** RENDER FUNCTION(S) **********/
+$(main);
 
-// This function conditionally replaces the contents of the <main> tag based on the state of the store
-
-/********** EVENT HANDLER FUNCTIONS **********/
-
-// These functions handle events (submit, click, etc)
